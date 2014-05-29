e930a0721acdbd8a22775820e044e811fb95204b
Emily Williams <ecwilliams66@gmail.com>
2014-05-29 12:29:34 -0400

    Deck and Card classes

--- blackjack.rb
+++ blackjack.rb
@@ -1,2 +1,30 @@
 class Deck
+  attr_reader :cards
+  SUITS  = ['c', 'h', 's', 'd']
+  VALUES = [2,3,4,5,6,7,8,9,10, 'J', 'Q', 'K', 'A']
+
+  def initialize
+    @cards = build_deck
+  end
+
+  def build_deck
+    deck = []
+    SUITS.each do |suit|
+      VALUES.each do |value|
+        deck << Card.new(value, suit)
+      end
+    end
+    deck
+  end
+
+  def self.shuffle
+    @cards.each do ||
+  end
+end
+
+class Card
+  def initialize(value, suit)
+    @value = value
+    @suit = suit
+  end
 end
