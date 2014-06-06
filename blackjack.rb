44771a8bab095fb6732152b365ab5fbb40f67401
Emily Williams <ecwilliams66@gmail.com>
2014-06-06 00:28:00 -0400

    All basic functionalities work, ends when a hand is over 21

--- blackjack.rb
+++ blackjack.rb
@@ -2,7 +2,7 @@ require 'pry'
 class Deck
   attr_reader :cards
   SUITS  = %w(H D S C)
-  RANKS = %w(A 1 2 3 4 5 6 7 8 9 10 J Q K)
+  RANKS = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
   VALUES = {
     'A' => 1,
     '2' => 2,
@@ -40,6 +40,10 @@ class Card
     @rank = rank
     @suit = suit
   end
+
+  def to_s
+    "#{rank}#{suit}"
+  end
 end
 
 class Hand
@@ -61,11 +65,7 @@ class Hand
   end
 
   def show_hand
-    cards = ""
-    self.hand_cards.each do |card|
-      cards << "#{card.rank}#{card.suit}"
-    end
-    cards
+    @hand_cards.join(' ')
   end
 end
 
@@ -81,57 +81,76 @@ class Game
     2.times{@dealer_hand.deal_from game_deck}
   end
 
+  def player_hand_value
+    player_hand.get_value
+  end
+
+  def dealer_hand_value
+    dealer_hand.get_value
+  end
+
+  def show_stats
+    print "--- Dealer Hand: #{dealer_hand.show_hand} --- "
+    puts "#{dealer_hand_value} "
+    print "--- Your Hand: #{player_hand.show_hand} --- "
+    puts "#{player_hand_value}"
+  end
+
   def player_prompt
     print "Stand or hit? "
     decision = gets.chomp
   end
 
-  def show_stats
-    print "--- Dealer Hand: "
-    puts dealer_hand.show_hand + " ---"
-    print "--- Value: "
-    puts "#{dealer_hand.get_value} ---"
-    print "--- Your Hand: "
-    puts player_hand.show_hand + " ---"
-    print "--- Value: "
-    puts "#{player_hand.get_value} ---"
-  end
-
-
-  def player_turn
+  def player_turn_under_21
     puts show_stats
-    decision = player_prompt
-    if decision == "hit"
-      player_hand.deal_from game_deck
-      player_turn
-    elsif decision == "stand"
-      player_hand
+    if player_hand_value > 21
+      return false
     else
-      puts 'Please enter "hit" or "stand"'
-      player_turn
+      decision = player_prompt
+      if decision == "stand"
+        return true
+      elsif decision == "hit"
+        player_hand.deal_from game_deck
+        player_turn_under_21
+      else
+        puts 'Please enter "hit" or "stand"'
+        player_turn_under_21
+      end
     end
   end
 
-  def dealer_turn
-    if dealer_hand.get_value < 17
-      dealer_hand.deal_from game_deck
-      dealer_turn
+  def dealer_turn_under_21
+    puts show_stats
+    if dealer_hand_value > 21
+      false
+    else
+      if dealer_hand_value < 17
+        dealer_hand.deal_from game_deck
+        dealer_turn_under_21
+      end
     end
+    true
   end
 
   def play_blackjack
-    player_turn
-    dealer_turn
-    show_stats
-    if player_hand.get_value > dealer_hand.get_value
-      puts "You win!"
-    elsif player_hand.get_value < dealer_hand.get_value
-      puts "You lose!"
+    if !player_turn_under_21
+      "You've exceeded 21! You lose :("
     else
-      puts "TIE."
+      if !dealer_turn_under_21
+        "You win!"
+      else
+        puts show_stats
+        if player_hand.get_value > dealer_hand.get_value
+          return "You win!"
+        elsif player_hand.get_value < dealer_hand.get_value
+          return "You lose!"
+        else
+          return "TIE."
+        end
+      end
     end
   end
 end
 
 new_game = Game.new
-new_game.play_blackjack
+puts new_game.play_blackjack
